d6309f8e24abf59ca25d5222c8fd2826a272a014
Vikas <vikaskumarbalbirsingh@gmail.com>
2019-06-21 12:16:35 +0530

    style changes

---
+++ src/components/ui/Carousel.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+type CarouselProps = {};
+type CarouselState = {};
+
+class CarouselHelper extends PureComponent<CarouselProps, CarouselState> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default CarouselHelper;
