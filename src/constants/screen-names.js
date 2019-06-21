9a604e8ba8b15a50d5d7c8b15f77dd99dc7be2e7
Vikas <vikaskumarbalbirsingh@gmail.com>
2019-06-21 11:50:40 +0530

    first screen routing

---
+++ src/constants/screen-names.js
@@ -0,0 +1,4 @@
+export const HOME = 'home';
+export const EXPLORE = 'explore';
+export const ACTOR = 'actor';
+export const DETAILS = 'details';
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-
+import Home from '../src/components/screens/home';
 import configureStore from './store';
 import './scss/_index.css';
 import App from './app';
+import { getCacheFilePath } from 'jest-haste-map';
+
+getPath(screenName){
+  return `/${screenName}`;
+}
 
 ReactDOM.render(
 	<Provider store={configureStore()}>
 		<Router>
 			<Route path="/" component={App}/>
+      <Route path={getPath(ScreenConstants.HOME) component = {Home}}/>
 		</Router>
 	</Provider>,
 	document.getElementById('root')
