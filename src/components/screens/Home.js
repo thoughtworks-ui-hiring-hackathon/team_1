4b932fef7d3390f0581774eb26fc0fafd12b4524
Vikas <vikaskumarbalbirsingh@gmail.com>
2019-06-21 13:00:49 +0530

    resolved merge-conflicts

---
+++ src/components/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../scss/style.scss';
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="carousal-container">
+          <h1>Latest</h1>
+        </div>
+        <div className="carousal-container">
+          <h1>Trending</h1>
+        </div>
+        <div className="carousal-container">
+          <h1>Most Watched</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore,applyMiddleware} from "redux";
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import Thunk from "redux-thunk";
+import Thunk from 'redux-thunk';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../src/components/screens/Home';
 import Explore from '../src/components/screens/Explore';
-import Reducer from "../src/reducers/root-reducer";
+import Reducer from '../src/reducers/root-reducer';
 import './scss/_index.css';
 import App from './app';
-import Navigation from "../src/components/Ui/Navigation";
-let store= createStore(Reducer,applyMiddleware(Thunk));
+import Navigation from '../src/components/Ui/Navigation';
 import * as ScreenConstants from './constants/Screen-names';
+let store = createStore(Reducer, applyMiddleware(Thunk));
 
 function getPath(screenName) {
   return `/${screenName}`;
@@ -19,7 +19,7 @@ function getPath(screenName) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <Navigation/>
+    <Navigation />
     <Router>
       <Route exact path="/" component={Home} />
       <Route
