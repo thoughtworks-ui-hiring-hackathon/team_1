a34b5ebe6e0301e75db9ce6090a026cba18b6352
sshejwal <sshejwal@qualys.com>
2019-06-21 11:55:17 +0530

    removed error in indexjs file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Home from '../src/components/screens/home';
+i
 import configureStore from './store';
 import './scss/_index.css';
 import App from './app';
@@ -15,8 +16,8 @@ getPath(screenName){
 ReactDOM.render(
 	<Provider store={configureStore()}>
 		<Router>
-			<Route path="/" component={App}/>
-      <Route path={getPath(ScreenConstants.HOME) component = {Home}}/>
+			<Route path="/" component={Home}/>
+      <Route path={getPath(ScreenConstants.HOME)} component = {E}/>
 		</Router>
 	</Provider>,
 	document.getElementById('root')
