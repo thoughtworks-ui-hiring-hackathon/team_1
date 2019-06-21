8b6a990ceb91d2035126f1ec9d226210aac5ef0c
Vikas <vikaskumarbalbirsingh@gmail.com>
2019-06-21 11:57:42 +0530

    resolved errors

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../src/components/screens/home';
 import configureStore from './store';
 import './scss/_index.css';
 import App from './app';
-import { getCacheFilePath } from 'jest-haste-map';
+import * as ScreenConstants from '../src/constants/screen-names';
 
-getPath(screenName){
+function getPath(screenName) {
   return `/${screenName}`;
 }
 
 ReactDOM.render(
-	<Provider store={configureStore()}>
-		<Router>
-			<Route path="/" component={App}/>
-      <Route path={getPath(ScreenConstants.HOME) component = {Home}}/>
-		</Router>
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={configureStore()}>
+    <Router>
+      <Route exact path="/" component={App} />
+      <Route exact path={getPath(ScreenConstants.HOME)} component={Home} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
